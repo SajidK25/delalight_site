af0586897f15ce28f5487ec42d0bf2da511b990f
Sajid25 <sajidk25@gmail.com>
2022-05-07 01:01:46 +0600

    url.py and setting.py  modified for PROD

--- delalight_site/urls.py
+++ delalight_site/urls.py
@@ -19,8 +19,17 @@ from django.views.generic import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns = [
+# DEV
+
+""" urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', include('home.urls')),
     path('', RedirectView.as_view(url='home/', permanent=True)),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+ """
+# PROD
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('home/', include('home.urls')),
+    path('', RedirectView.as_view(url='home/', permanent=True)),
+]
